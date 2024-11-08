860b29c2fdf55f9d2a19e1f2b66478fd4d874643
eneekle <iameneekle@gmail.com>
2024-11-08 17:27:15 -0600

    s3.tf

--- s3.tf
+++ s3.tf
@@ -1,8 +1,9 @@
-resource "aws_s3_bucket" "jenkins-1" {
-  bucket = "jenkins-1"
-
-  tags = {
-    Name        = "jenkins-1"
-    Environment = "Dev"
-  }
-}
+# resource "aws_s3_bucket" "jenkins-1" {
+  # bucket = "jenkins-1"
+# 
+  # tags = {
+    # Name        = "jenkins-1"
+    # Environment = "Dev"
+  # }
+# }
+# 
