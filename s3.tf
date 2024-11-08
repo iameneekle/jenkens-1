3c0619c4fa944b3777a2f477e112143187360cb2
iameneekle <165346090+iameneekle@users.noreply.github.com>
2024-11-07 19:44:33 -0600

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,8 +1,8 @@
 resource "aws_s3_bucket" "web-app2-faith-bucket" {
-  bucket = "web-app2-faith-bucket"
+  bucket = "web-app2-faith-bucket2"
 
   tags = {
-    Name        = "web-app2-faith-bucket"
+    Name        = "web-app2-faith-bucket2"
     Environment = "Dev"
   }
 }
